72b3de7bd01690103f77795812c8dc911246477d
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-04 00:39:58 +0700

    Update something

--- MyApplication/app/src/androidTest/java/com/demo/myapplication/ApplicationTest.java
+++ MyApplication/app/src/androidTest/java/com/demo/myapplication/ApplicationTest.java
@@ -1,13 +1,30 @@
 package com.demo.myapplication;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.test.ActivityInstrumentationTestCase2;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity mainActivity;
+
     public ApplicationTest() {
-        super(Application.class);
+        super(MainActivity.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mainActivity = getActivity();
+    }
+
+    public void testPreconditions() {
+        assertEquals(1, 1);
+    }
+
+    public void testPreconditions2() {
+        assertEquals(1, 1);
+    }
+
 }
